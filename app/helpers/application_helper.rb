4c898fd2e72e4c58a8c98e4f26e11e4339702199
Richard Hurt <rnhurt@kangaroobox.com>
2011-03-27 06:23:02 -0400

    Flash messages are now working fairly well

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   # Show the FLASH div if there is data in the flash object.
   def show_flash
     result = ''
-    flash.each {|type, message| result << content_tag(:div, message, :id => 'notice', :class => "#{type.to_s} ui-state-highlight ui-corner-all") }
+    flash.each {|type, message| result << content_tag(:div, message, :class => "#{type.to_s} close") }
     return result.html_safe
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,10 @@
-$(function() {    
+$(function() {
+  // Make the tables a bit prettier
   $('.tablesorter').tablesorter({widgets: ['zebra']});
+
+  // Make the flash notices a bit prettier
+  $('#flash .close').click(function() {
+    $(this).animate({height: 'hide'}, 'fast');
+  });
+
 }); 

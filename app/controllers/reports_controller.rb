33781671ab2bcfcedc2b1a28ecca03e30a70fa5d
Richard Hurt <rnhurt@kangaroobox.com>
2011-03-30 05:52:11 -0400

    Added Search & Admin skeletons

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,7 @@ class ReportsController < ApplicationController
   def show
     case params[:id]
       when "recent_contacts"
-        @contacts = Contact.recent(10)
+        @contacts = Contact.recent(params[:q])
         render "recent_contacts"
       else
         redirect_to reports_url, :flash => { :warning => "Report not found" }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Mfp::Application.routes.draw do
   resources :reports
   resources :contacts
+  resources :search
+  resources :admin
 
   root :to => "dashboard#index"
 end

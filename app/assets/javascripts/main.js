51760ad4c10f94c103bb889b44787d82490bc85c
Ian Massey <imassey@cafepress.com>
2011-10-08 14:29:46 -0400

    header moved to sidebar, behaviors (expand/collapse), fixed double row for dashboard buttons, optimized for 800x600 resolution

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,8 +9,40 @@ mfp = window.mfp || {};
 // initialization...
 // anything that needs to run globally on page load should go inside this function
 mfp.init = function(){
+  
+  // handle flash notices
   var flash = $('#flash');
   if( flash.html().match(/\w/) ){
     flash.slideDown();
+  }
+  
+  // header collapse on load
+  mfp.header.init();
+  
+};
+
+// header collapse and expand behaviors
+mfp.header = {
+  init: function(){
+    var headerCollapseTimer = window.setTimeout(mfp.header.collapse, 1500);
+    $('#header_mini').live('mouseenter', function(){
+      $(this).slideUp('fast', function(){
+        mfp.header.expand();
+      });
+    });
+    $('#header').live('mouseleave', function(e){
+      e.stopPropagation();
+      mfp.header.collapse();
+    });
+  },
+  
+  collapse: function(){
+    $('#header').slideUp('fast', function(){
+      $('#header_mini').slideDown('fast');
+    });
+  },
+  
+  expand: function(){
+    $('#header').slideDown('fast');
   }
 };

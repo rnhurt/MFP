33b4983ab17df6cab1ad6f1f13bbc7b5d9449ab4
Ian Massey <imassey@cafepress.com>
2011-10-23 18:15:06 -0400

    update schema from migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111023212838) do
+ActiveRecord::Schema.define(:version => 20111023214514) do
 
   create_table "addresses", :force => true do |t|
     t.string   "street_number"

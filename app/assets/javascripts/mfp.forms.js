e184fbc60f66abe18d74394d89a88c321e471dc1
Ian Massey <imassey@cafepress.com>
2011-11-14 22:10:09 -0500

    text input behaviors for new/edit forms

--- app/assets/javascripts/mfp.forms.js
+++ app/assets/javascripts/mfp.forms.js
@@ -6,7 +6,24 @@ mfp.forms = {
   
   // corresponds with "new" actions on controllers
   new: {
+    
     init: function(){
+      
+      // text input clear and default
+      $('input[type="text"]').each(function(){
+        $(this).data('default', $(this).val())
+          .addClass('default-text');
+      }).click(function(){
+        if( $(this).val() == $(this).data('default') ){
+          $(this).val('')
+            .removeClass('default-text');
+        }
+      }).blur(function(){
+        if( $(this).val() == '' ){
+          $(this).val($(this).data('default'))
+            .addClass('default-text');
+        }
+      });
       
     }
   }
